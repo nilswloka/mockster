fe662bfd5d07ffc7b55c9e32a61ea99473ceacde
Nils Wloka <mail@nilswloka.com>
2012-09-04 14:58:53 +0200

    Changed version number.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
-(defproject mockster "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject mockster "0.1.0"
+  :description "A simple, configurable data backend mock."
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
